c3e149671473de9a1d1946c848570130b072343b
Matthew <83830727+Marfolo@users.noreply.github.com>
2022-11-26 11:22:28 +0100

    Update game.py
    Second

--- game.py
+++ game.py
@@ -3,15 +3,15 @@ import random
 
 def next_turn():
     pass
-    
+
 def check_winner():
     pass
-    
+
  def empty_spaces():
     pass
-    
+
 def new_game():
     pass
-    
+
 window = Tk()
 window.mainloop()
